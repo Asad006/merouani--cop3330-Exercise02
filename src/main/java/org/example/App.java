3f7cc10dc116302bbba92b0740a5a198c7decff8
meroa006 <merouani.ass@gmail.com>
2021-06-06 10:42:04 -0400

    Initial commit

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,46 @@
+package org.example;
+
+import java.util.Scanner;
+
+/**
+ * Exercise 2 - Counting the Number of Characters
+ * Create a program that prompts for an input string and displays output that shows the input string and the number of characters the string contains.
+ * Example Output
+ * What is the input string? Homer
+ * Homer has 5 characters.
+ * Constraints
+ * Be sure the output contains the original string.
+ * Use a single output statement to construct the output.
+ * Use a built-in function of the programming language to determine the length of the string.
+ * Challenges
+ * If the user enters nothing, state that the user must enter something into the program.
+ * Implement this program using a graphical user interface and update the character counter every time a key is pressed. If your language doesn’t have a particularly friendly GUI library, try doing this exercise with HTML and JavaScript instead.
+ */
+public class App {
+    public static void main(String[] args) {
+        App app = new App();
+        Scanner sc = new Scanner(System.in);
+
+        String input;
+        int stringLength;
+
+        input = app.getInput(sc);
+        stringLength = app.getStringLength(input);
+        app.printResults(stringLength,input);
+
+    }
+
+    private  void printResults(int length, String input) {
+        System.out.printf("%s  has %d characters.", input, length);
+    }
+
+    private  int getStringLength(String s) {
+        return s.length();
+    }
+
+    private  String getInput(Scanner sc) {
+        System.out.println("What is the input string?");
+        String input = sc.nextLine();
+        return input;
+    }
+}
