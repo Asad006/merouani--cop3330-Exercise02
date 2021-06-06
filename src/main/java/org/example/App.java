1c6ac9a89fa5de42b7ac919e44a184514467bbab
meroa006 <merouani.ass@gmail.com>
2021-06-06 12:11:19 -0400

    copy

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -5,10 +5,7 @@
 package org.example;
 
 import java.util.Scanner;
-/*
- *  UCF COP3330 Summer 2021 Assignment 1 Solution
- *  Copyright 2021 Asad merouani
- */
+
 public class App {
     public static void main(String[] args) {
         App app = new App();
